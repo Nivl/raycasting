7b213090305494a5451e59c313efa78243d4f901
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-02-17 02:26:18 -0800

    Add grass on default view

--- src/app/raycasting.js
+++ src/app/raycasting.js
@@ -3,17 +3,23 @@ import WMap from './map/w-map.js';
 export default class Raycasting {
   constructor() {
     const canvas = document.getElementById('glcanvas');
-    this.width = canvas.width;
-    this.height = canvas.height;
     this.ctx = null;
     this.map = null;
+    this.screen = {
+      w    : canvas.width,
+      h    : canvas.height,
+      halfW: canvas.width / 2,
+      halfH: canvas.height / 2,
+    };
 
     if (canvas.getContext) {
       this.ctx = canvas.getContext('2d');
 
       // Set default color
       this.ctx.fillStyle = '#87CEFA';
-      this.ctx.fillRect(0, 0, this.width, this.height);
+      this.ctx.fillRect(0, 0, this.screen.w, this.screen.h);
+      this.ctx.fillStyle = '#C5E3BF';
+      this.ctx.fillRect(0, this.screen.halfH, this.screen.w, this.screen.h);
     }
   }
 
