b72d3fe8e76b258e61ff640d5401f1bd51db88c3
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-02-18 03:20:28 -0800

    Move user

--- src/app/main.js
+++ src/app/main.js
@@ -5,10 +5,10 @@ const raycasting = new Raycasting();
 if (raycasting.hasSupport()) {
   raycasting.loadMap([
     [1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 2, 0, 1],
     [1, 0, 0, 0, 1, 1],
     [1, 1, 0, 1, 0, 1],
-    [1, 0, 2, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1],
   ]);
   raycasting.start();
