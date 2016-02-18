b6b23d9974179f19d7c7de36fab34443cd863323
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-02-18 03:20:38 -0800

    Draw walls

--- src/app/raycasting.js
+++ src/app/raycasting.js
@@ -1,10 +1,13 @@
 import WMap from './map/w-map.js';
+import WMapTypes from './map/w-map-types';
+import Camera from './camera.js';
 
 export default class Raycasting {
   constructor() {
     const canvas = document.getElementById('glcanvas');
     this.ctx = null;
     this.map = null;
+    this.camera = null;
     this.screen = {
       w    : canvas.width,
       h    : canvas.height,
@@ -29,6 +32,7 @@ export default class Raycasting {
 
   start() {
     if (this.map) {
+      this.camera = new Camera(this.map.findFirst(WMapTypes.CAMERA), this.screen);
       this.draw();
     } else {
       throw new Error('map is missing');
@@ -40,6 +44,41 @@ export default class Raycasting {
   }
 
   draw() {
+    for (let i = 0; i < this.screen.w; i++) {
+      const vector = this.camera.geViewVector(i);
 
+      let k = -1;
+      for (let x = 0; x < this.map.w; x++) {
+        const newK = (x - this.camera.pos[0]) / vector.x;
+        const y = this.camera.pos[1] + k * vector.y;
+
+        if (newK > 0) {
+          if (this.map.findAt(x, ~~y) === WMapTypes.WALL) {
+            if (k < 0 || newK < k) {
+              k = newK;
+            }
+          }
+        }
+      }
+
+      for (let y = 0; y < this.map.h; y++) {
+        const newK = (y - this.camera.pos[1]) / vector.y;
+        const x = this.camera.pos[0] + k * vector.x;
+
+        if (newK > 0) {
+          if (this.map.findAt(~~x, y) === WMapTypes.WALL) {
+            if (k < 0 || newK < k) {
+              k = newK;
+            }
+          }
+        }
+      }
+
+      if (k > -1) {
+        const wallheight = this.screen.h / (2 * k);
+        this.ctx.fillStyle = 'rgb(200,0,0)';
+        this.ctx.fillRect(i, (this.screen.halfH - wallheight), 1, wallheight * 2);
+      }
+    }
   }
 }
